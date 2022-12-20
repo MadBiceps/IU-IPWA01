7ac4b49030f193814db4bd626aa17a349a308886
Fabian Fischer <fabian.niklas.fischer@googlemail.com>
2022-12-20 17:52:41 +0100

    Add header area

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas, faTshirt } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faTshirt);
+  }
+}
