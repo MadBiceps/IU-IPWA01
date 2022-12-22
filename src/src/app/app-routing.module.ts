6904d124726b812afb1bdfb774870ca026cc0afe
Fabian Fischer <fabian.niklas.fischer@gmail.com>
2022-12-22 09:29:55 +0100

    Implement impressum page

--- src/src/app/app-routing.module.ts
+++ src/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImpressumPageComponent } from './pages/impressum-page/impressum-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'impressum',
+    component: ImpressumPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { fas, faTshirt } from '@fortawesome/free-solid-svg-icons';
+import { ImpressumPageComponent } from './pages/impressum-page/impressum-page.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, ImpressumPageComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
