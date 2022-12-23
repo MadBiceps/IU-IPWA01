472bf1ab4edf984162f147ef402a355285f61157
Fabian Fischer <fabian.niklas.fischer@gmail.com>
2022-12-23 08:42:10 +0100

    Removed cookie references (#12)

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { fas, faTshirt } from '@fortawesome/free-solid-svg-icons';
+import { faTshirt } from '@fortawesome/free-solid-svg-icons';
 import { ImprintPageComponent } from './pages/imprint-page/imprint-page.component';
 import { PrivacyPageComponent } from './pages/privacy-page/privacy-page.component';
 
